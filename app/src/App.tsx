d63629aca64857b57db754244cd87aa987c032f1
Kaue Lunelli <lunellikaue@gmail.com>
2024-08-10 21:52:21 -0300

    chore: Update ESLint and PostCSS dependencies

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import './App.css';
-import { LoginPage } from './pages/login'
-import { HomePage } from './pages/home'
-import { SingUpPage } from './pages/singnup'
+import { LoginPage } from "./pages/login";
+import { HomePage } from "./pages/home";
+import { SingUpPage } from "./pages/singnup";
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([{
-  path: "/",
-  element: <LoginPage />
-}, {
-  path: '/singup',
-  element: <SingUpPage />
-}, {
-  path: '/home',
-  element: <HomePage />
-}
-])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+  },
+  {
+    path: "/singup",
+    element: <SingUpPage />,
+  },
+  {
+    path: "/home",
+    element: <HomePage />,
+  },
+]);
 
 export function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
 export default App;
